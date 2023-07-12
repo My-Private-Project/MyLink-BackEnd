d8b028ea9343a0f83e6a3f46fac04fbf537e8c08
Rodrigodcs <rodrigodcsee@gmail.com>
2023-07-12 16:04:45 -0300

    Code review

--- src/schemas/index.ts
+++
@@ -1,5 +0,0 @@
-export * from './users-schemas';
-export * from './authentication-schemas';
-export * from './card-schemas';
-export * from './link-schemas';
-export * from './body-schemas';
--- src/schemas/users-schemas.ts
+++
@@ -1,8 +0,0 @@
-import Joi from 'joi';
-import { CreateUserParams } from '@/services/users-service';
-
-export const createUserSchema = Joi.object<CreateUserParams>({
-  email: Joi.string().email().required(),
-  name: Joi.string().min(3).required(),
-  password: Joi.string().min(6).required(),
-});
